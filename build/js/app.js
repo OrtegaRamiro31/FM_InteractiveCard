2f9361092c5192ebe797112dac11b5598f9841ec
OrtegaRamiro31 <ceinnova13458@innovaccion.mx>
2023-01-10 23:03:58 -0600

    Comportamiento con JS terminado. Aplicación funcionando.

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,113 @@
-console.log("sad");
+import { verifyCardName, verifyCardNumber, monthAndYearVerify, cvcVerify } from "./helper.js";
+
+// ****************Constantes**************** //
+/* Main Container */
+const mainContainer = document.querySelector('.main-container__form-section');
+/* CardHolder */
+const showErrorCardHolder = document.querySelector(".form__cardholder--error");
+const inputCardName = document.querySelector('#cardholder');
+const cardName = document.querySelector('.card__details-name');
+
+/* CardNumber */
+const showErrorCardNum = document.querySelector('.form__inputnumber--error');
+const inputCardNum = document.querySelector('#cardNumber');
+const cardNumber = document.querySelector('.card__number');
+
+/* CardDates */
+const showErrorDates = document.querySelector('.from__input-mmyy--error');
+const inputMonth = document.querySelector('#cardMonth');
+const inputYear = document.querySelector('#cardYear');
+const cardMonth = document.querySelector('.card__month');
+const cardYear = document.querySelector('.card__year');
+
+/* CVC */
+const showErrorCardCVC = document.querySelector('.from__input-cvc--error');
+const inputCVC = document.querySelector('#cardCvc');
+const cardCVC = document.querySelector('.card-back__cvc');
+
+/* Submit */
+const inputSubmitInfo = document.querySelector(".form__submit");
+
+/* Thanks Container */
+const thanks_section = document.querySelector('.thanks-section');
+const inputContinue = document.querySelector('.thanks-section__button');
+/* Funcion Inicial */
+document.addEventListener('DOMContentLoaded', () => {
+    iniciarApp();
+});
+
+function iniciarApp(){
+    cardHolder();
+    cardNum();
+    cardDate();
+    backCardCVC();
+    inputSubmit();
+}
+
+function cardHolder() {
+    inputCardName.addEventListener('input', (e) => {
+
+        cardName.textContent = e.target.value;
+        
+    });
+}
+
+function cardNum() {
+
+    inputCardNum.addEventListener('input', (e) => {
+        let strValueNumber;
+        strValueNumber = e.target.value
+        // Eliminamos espacios en blanco
+        .replace(/\s/g, '')
+        // Ponemos espacio cada cuatro/letras numeros
+        .replace(/([0-9 a-z A-Z]{4})/g, '$1 ')
+        // Elimina el ultimo espaciado
+        .trim();
+
+        e.target.value = strValueNumber;
+
+        cardNumber.textContent = strValueNumber;
+    });
+}
+
+function cardDate() {
+
+    inputMonth.addEventListener('input', (e) => {
+        cardMonth.textContent = e.target.value;
+    });
+
+    inputYear.addEventListener('input', (e) => {
+        
+        cardYear.textContent = e.target.value;
+    });
+}
+
+function backCardCVC(){
+    inputCVC.addEventListener('input', (e) => {
+        cardCVC.textContent = e.target.value;
+    });
+}
+
+function inputSubmit() {
+    
+    inputSubmitInfo.addEventListener('click', (e) => {
+        e.preventDefault();
+        verifyInputs();
+    });
+}
+
+function verifyInputs(){
+    if(    
+        verifyCardName(inputCardName, showErrorCardHolder),
+        verifyCardNumber(inputCardNum, showErrorCardNum),
+        monthAndYearVerify(inputMonth, inputYear, showErrorDates),
+        cvcVerify(inputCVC, showErrorCardCVC)
+        ) {
+            console.log(thanks_section);
+            thanks_section.style.display="flex";
+            mainContainer.style.display="none";
+            inputContinue.addEventListener('click', () => {
+                location.reload();
+            });
+        } 
+}
---
+++ build/js/helper.js
@@ -0,0 +1,105 @@
+function verifyOnlyNumbers(numero) {
+    const regex = /^[0-9]*$/;
+    return regex.test(numero);
+} 
+
+function verifyCardName(inputCardName, showErrorCardHolder){
+    const regex = /^[a-z A-Z]*$/;
+    
+    // If input is empty
+    if(inputCardName.value.length === 0) {
+        showErrorCardHolder.textContent = 'Can\'t be blank';
+        inputCardName.classList.add('error-input');
+    }
+    // All less letters
+    else if(!regex.test(inputCardName.value)) {
+        showErrorCardHolder.textContent = "Wrong format, letters only";
+        inputCardName.classList.add('error-input');
+    }
+
+    else {
+        showErrorCardHolder.textContent = "";
+        inputCardName.classList.remove('error-input');
+        return true;
+    }
+    return false;
+}
+
+function verifyCardNumber(inputCardNum, showErrorCardNum) {
+    // If input is empty
+    if(inputCardNum.value.length === 0) { 
+        showErrorCardNum.textContent = 'Can\'t be blank';
+        inputCardNum.classList.add('error-input');
+    }
+
+    // If input contains letters or others characters
+    else if(!verifyOnlyNumbers(inputCardNum.value.split(" ").join(""))){
+        showErrorCardNum.textContent = 'Wrong format, numbers only';
+        inputCardNum.classList.add('error-input');
+    }
+
+    else if(inputCardNum.value.split(" ").join("").length < 16) {
+        showErrorCardNum.textContent = 'Can\'t be less than 16 numbers';
+        inputCardNum.classList.add('error-input');
+    }
+
+    else {  
+        showErrorCardNum.textContent = '';
+        inputCardNum.classList.remove('error-input');
+        return true;
+    }
+    return false;
+}
+
+function monthAndYearVerify(inputMonth, inputYear, showErrorDates){
+    let currentYear = parseInt(new Date().getFullYear().toString().slice(2,4));
+    if(inputMonth.value.length === 0 || inputYear.value.length === 0) {
+        showErrorDates.textContent = 'Can\'t be blank';
+        inputMonth.classList.add('error-input');
+        showErrorDates.textContent = 'Can\'t be blank';
+        inputYear.classList.add('error-input');
+    }
+    else if(inputMonth.value.length === 0) {
+        showErrorDates.textContent = 'Can\'t be blank';
+        inputMonth.classList.add('error-input');
+    }
+    else if( inputYear.value.length === 0){
+        showErrorDates.textContent = 'Can\'t be blank';
+        inputYear.classList.add('error-input');
+
+    }
+    else if(inputMonth.value > 12 || !verifyOnlyNumbers(inputMonth.value) || inputMonth.value.length < 2 || !verifyOnlyNumbers(inputYear.value) || inputYear.value.length < 2 || inputYear.value < currentYear) {
+        inputMonth.classList.add('error-input');
+        showErrorDates.textContent = "Invalid Month Or Year";
+    }
+    else {
+        inputMonth.classList.remove('error-input');
+        inputYear.classList.remove('error-input');
+        showErrorDates.textContent = "";
+        return true;
+    }
+    return false;
+}
+
+function cvcVerify(inputCVC, showErrorCardCVC){
+    if(inputCVC.value === 0) {
+        showErrorCardCVC.textContent = 'Can\'t be blank';
+        inputCVC.classList.add('error-input');
+    } 
+    else if(inputCVC.value.length < 3) {
+        showErrorCardCVC.textContent = 'Can\'t be less than 3 numbers';
+        inputCVC.classList.add('error-input');
+    } 
+    else if(!verifyOnlyNumbers(inputCVC.value)){
+        showErrorCardCVC.textContent = 'Invalid CVC';
+        inputCVC.classList.add('error-input');
+    }
+    else {
+        inputCVC.classList.remove('error-input');
+        showErrorCardCVC.textContent = '';
+        return true;
+    }
+    return false;
+}
+
+export {verifyOnlyNumbers, verifyCardName, verifyCardNumber, monthAndYearVerify, cvcVerify};
